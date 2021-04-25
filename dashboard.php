a64e6fe70cd37715d47c3fbf773604e484556c8f
uchario <77892510+uchario@users.noreply.github.com>
2021-04-25 18:26:10 +0100

    added button class to glyphicons to give padding

--- dashboard.php
+++ dashboard.php
@@ -78,9 +78,9 @@ require_once "head.php";
                                 echo "<tr>";
                                     echo "<td>" . $row['name'] . "</td>";
                                     echo "<td>";
-                                        echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-                                        echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                                        echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip' class='btn btn-default'><span class='glyphicon glyphicon-eye-open '></span></a>";
+                                        echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip' class='btn btn-default'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                        echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip' class='btn btn-default'><span class='glyphicon glyphicon-trash'></span></a>";
                                     echo "</td>";
                                 echo "</tr>";
                             }
